b693a73593a6ec41de86697a1a51b7621a556afc
geitsi <attila@geitsi.com>
2022-11-11 16:45:18 +0100

    conflict

--- UserMaintenance/Resource1.Designer.cs
+++ UserMaintenance/Resource1.Designer.cs
@@ -22,7 +22,7 @@ namespace UserMaintenance {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resource1 {
+    public class Resource1 {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace UserMaintenance {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("UserMaintenance.Resource1", typeof(Resource1).Assembly);
@@ -51,7 +51,7 @@ namespace UserMaintenance {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -63,7 +63,7 @@ namespace UserMaintenance {
         /// <summary>
         ///   Looks up a localized string similar to Hozzáadás.
         /// </summary>
-        internal static string Add {
+        public static string Add {
             get {
                 return ResourceManager.GetString("Add", resourceCulture);
             }
@@ -72,9 +72,18 @@ namespace UserMaintenance {
         /// <summary>
         ///   Looks up a localized string similar to Név:.
         /// </summary>
-        internal static string FullName {
+        public static string FullName {
             get {
                 return ResourceManager.GetString("FullName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Fájlba írás.
+        /// </summary>
+        public static string WriteFile {
+            get {
+                return ResourceManager.GetString("WriteFile", resourceCulture);
             }
         }
     }
