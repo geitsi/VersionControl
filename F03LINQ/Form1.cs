440ffd3f2ef76424dbaa06dbc5a807c891e8c9bb
geitsi <attila@geitsi.com>
2022-11-18 15:40:22 +0100

    Done

--- F03LINQ/Form1.cs
+++ F03LINQ/Form1.cs
@@ -5,10 +5,13 @@ namespace F03LINQ
     public partial class Form1 : Form
     {
         private List<Country> countries = new List<Country>();
+        private List<Ramen> ramens = new List<Ramen>();
         public Form1()
         {
             InitializeComponent();
             LoadData("ramen.csv");
+            listCountries.DisplayMember = "Name";
+            GetCountries();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -58,5 +61,49 @@ namespace F03LINQ
 
             return currentCountry;
         }
+        private void GetCountries()
+        {
+            var countriesList = from c in countries
+                                where c.Name.ToUpper().StartsWith(txtCountryFilter.Text.ToUpper())
+                                orderby c.Name
+                                select c;
+            listCountries.DataSource = countriesList.ToList();
+        }
+        private void txtCountryFilter_TextChanged(object sender, EventArgs e)
+        {
+            GetCountries();
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listCountries_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        
+                var country = (Country)((ListBox)sender).SelectedItem;
+                if (country == null)
+                    return;
+
+                var countryRamens = from r in ramens
+                                    where r.CountryFK == country.ID
+                                    select r;
+
+                var groupedRamens = from r in countryRamens
+                                    group r.Stars by r.Brand into g
+                                    select new
+                                    {
+                                        BrandName = g.Key,
+                                        AverageRating = Math.Round(g.Average(), 2)
+                                    };
+
+                var orderedGroups = from g in groupedRamens
+                                    orderby g.AverageRating descending
+                                    select g;
+
+                dgwRamen.DataSource = orderedGroups.ToList();
+        
+        } 
     }
 }
