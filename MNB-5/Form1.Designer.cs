5dbd3ba3179feeeca61f01e9344e3252a9bb6720
geitsi <attila@geitsi.com>
2022-12-16 13:52:49 +0100

    felx

--- MNB-5/Form1.Designer.cs
+++ MNB-5/Form1.Designer.cs
@@ -28,21 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.comboBoxCurrency = new System.Windows.Forms.ComboBox();
             this.dateTimePickerEnd = new System.Windows.Forms.DateTimePicker();
             this.dateTimePickerStart = new System.Windows.Forms.DateTimePicker();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(160, 179);
+            this.dataGridView1.Location = new System.Drawing.Point(12, 65);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(445, 497);
             this.dataGridView1.TabIndex = 0;
             // 
             // comboBoxCurrency
@@ -77,11 +82,28 @@
             this.dateTimePickerStart.Size = new System.Drawing.Size(265, 22);
             this.dateTimePickerStart.TabIndex = 7;
             // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(477, 65);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(616, 497);
+            this.chart1.TabIndex = 8;
+            this.chart1.Text = "chart1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1063, 533);
+            this.ClientSize = new System.Drawing.Size(1105, 574);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.dateTimePickerStart);
             this.Controls.Add(this.dateTimePickerEnd);
             this.Controls.Add(this.comboBoxCurrency);
@@ -90,6 +112,7 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -100,6 +123,7 @@
         private System.Windows.Forms.ComboBox comboBoxCurrency;
         private System.Windows.Forms.DateTimePicker dateTimePickerEnd;
         private System.Windows.Forms.DateTimePicker dateTimePickerStart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- MNB-5/Form1.cs
+++ MNB-5/Form1.cs
@@ -5,10 +5,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace MNB_5
@@ -21,6 +23,7 @@ namespace MNB_5
         {
             InitializeComponent();
             dataGridView1.DataSource = Rates;
+            chart1.DataSource = Rates;
             comboBoxCurrency.DataSource = Currencies;
             GetData();
         }
@@ -43,6 +46,73 @@ namespace MNB_5
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+        public string Req()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = comboBoxCurrency?.SelectedItem?.ToString() ?? "EUR",
+                startDate = dateTimePickerStart.Value.ToString("yyyy-MM-dd"),
+                endDate = dateTimePickerEnd.Value.ToString("yyyy-MM-dd")
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+            return result;
+        }
+        public void RefreshData()
+        {
+            Rates.Clear();
+
+            string result = Req();
+            xml(result);
+
+            var series = chart1.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chart1.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chart1.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+        }
+        public void xml(string result)
+        {
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(result);
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                XmlElement childElement = ((XmlElement)element.ChildNodes[0]);
+
+                if (childElement == null)
+                {
+                    continue;
+                }
+
+                decimal unit = decimal.Parse(childElement.GetAttribute("unit"));
+
+                NumberFormatInfo numberFormatWithComma = new NumberFormatInfo();
+                numberFormatWithComma.NumberDecimalSeparator = ",";
+
+                decimal value = Math.Round(decimal.Parse(childElement.InnerText, numberFormatWithComma), 2);
+
+                RateData rate = new RateData
+                {
+                    Date = Convert.ToDateTime(element.GetAttribute("date")),
+                    Currency = childElement.GetAttribute("curr"),
+                    Value = unit != 0 ? value / unit : value,
+                };
+
+                Rates.Add(rate);
+            }
         }
     }
 }
