1294e3d69742b0b1fdaa67c9853488e90e50ea4b
geitsi <attila@geitsi.com>
2022-12-16 13:44:10 +0100

    fel5

--- MNB-5/Form1.Designer.cs
+++ MNB-5/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBoxCurrency = new System.Windows.Forms.ComboBox();
+            this.dateTimePickerEnd = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerStart = new System.Windows.Forms.DateTimePicker();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -42,11 +45,46 @@
             this.dataGridView1.Size = new System.Drawing.Size(240, 150);
             this.dataGridView1.TabIndex = 0;
             // 
+            // comboBoxCurrency
+            // 
+            this.comboBoxCurrency.FormattingEnabled = true;
+            this.comboBoxCurrency.Items.AddRange(new object[] {
+            "EUR",
+            "USD",
+            "PLN",
+            "CHF",
+            "GBP"});
+            this.comboBoxCurrency.Location = new System.Drawing.Point(617, 11);
+            this.comboBoxCurrency.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBoxCurrency.Name = "comboBoxCurrency";
+            this.comboBoxCurrency.Size = new System.Drawing.Size(160, 24);
+            this.comboBoxCurrency.TabIndex = 5;
+            this.comboBoxCurrency.Text = "EUR";
+            // 
+            // dateTimePickerEnd
+            // 
+            this.dateTimePickerEnd.Location = new System.Drawing.Point(325, 13);
+            this.dateTimePickerEnd.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePickerEnd.Name = "dateTimePickerEnd";
+            this.dateTimePickerEnd.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePickerEnd.TabIndex = 6;
+            // 
+            // dateTimePickerStart
+            // 
+            this.dateTimePickerStart.Location = new System.Drawing.Point(13, 13);
+            this.dateTimePickerStart.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePickerStart.Name = "dateTimePickerStart";
+            this.dateTimePickerStart.Size = new System.Drawing.Size(265, 22);
+            this.dateTimePickerStart.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1063, 533);
+            this.Controls.Add(this.dateTimePickerStart);
+            this.Controls.Add(this.dateTimePickerEnd);
+            this.Controls.Add(this.comboBoxCurrency);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -59,6 +97,9 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBoxCurrency;
+        private System.Windows.Forms.DateTimePicker dateTimePickerEnd;
+        private System.Windows.Forms.DateTimePicker dateTimePickerStart;
     }
 }
 
--- MNB-5/Form1.cs
+++ MNB-5/Form1.cs
@@ -9,27 +9,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace MNB_5
 {
     public partial class Form1 : Form
     {
         BindingList<RateData> Rates = new BindingList<RateData>();
+        BindingList<string> Currencies = new BindingList<string>();
         public Form1()
         {
             InitializeComponent();
             dataGridView1.DataSource = Rates;
-            var mnbService = new MNBArfolyamServiceSoapClient();
-            var request = new GetExchangeRatesRequestBody()
-            {
-                currencyNames = "EUR",
-                startDate = "2020-01-01",
-                endDate = "2020-06-30"
-            };
-            var response = mnbService.GetExchangeRates(request);
-            var result = response.GetExchangeRatesResult;
+            comboBoxCurrency.DataSource = Currencies;
+            GetData();
         }
+        void GetData()
+        {
+            var client = new MNBArfolyamServiceSoapClient();
+            var requestBody = new GetCurrenciesRequestBody();
 
+            var response = client.GetCurrencies(requestBody);
+
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(response.GetCurrenciesResult);
+            Console.WriteLine(response.GetCurrenciesResult);
+            foreach (XmlElement element in xml.DocumentElement.ChildNodes[0])
+            {
+                Currencies.Add(element.InnerText);
+            }
+            comboBoxCurrency.DataSource = Currencies;
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
 
