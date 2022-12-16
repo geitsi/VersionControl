ac91ebce190006bfd90566ada3ef390fee171431
geitsi <attila@geitsi.com>
2022-12-16 13:03:32 +0100

    fel1

--- MNB-5/Form1.cs
+++ MNB-5/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MNB_5.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,20 @@ namespace MNB_5
         public Form1()
         {
             InitializeComponent();
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
