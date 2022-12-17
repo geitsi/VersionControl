c1734881ef98605b87219a17601b61a39742cdce
geitsi <attila@geitsi.com>
2022-12-17 13:38:38 +0100

    fel5

---
+++ week07/Entities/PortfolioItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week07.Entities
+{
+    public class PortfolioItem
+    {
+        public decimal Volume { get; set; }
+        public string Index { get; set; }
+    }
+}
--- week07/Form1.cs
+++ week07/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week07.Entities;
 
 namespace week07
 {
@@ -16,20 +17,29 @@ namespace week07
 
         PortfolioEntities context = new PortfolioEntities();
 
-        //List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
 
         List<decimal> Nyereségek = new List<decimal>();
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Tick.ToList();
-
+            
             dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
