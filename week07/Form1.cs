203f1ef7541735019ae3a1fa8d90b6a7e9b5709f
geitsi <attila@geitsi.com>
2022-12-17 13:33:38 +0100

    fel4

--- week07/Form1.cs
+++ week07/Form1.cs
@@ -12,9 +12,24 @@ namespace week07
 {
     public partial class Form1 : Form
     {
+        List<Tick> Ticks;
+
+        PortfolioEntities context = new PortfolioEntities();
+
+        //List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+
+        List<decimal> Nyereségek = new List<decimal>();
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Tick.ToList();
+
+            dataGridView1.DataSource = Ticks;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
